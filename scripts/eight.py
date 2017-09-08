6aaa9a14749c5292a6301584e710345b77e19b9f
Kallekro <kallekro@gmail.com>
2017-09-08 10:44:04 +0200

    Kommentarer i koden

---
+++ scripts/eight.py
@@ -0,0 +1,29 @@
+import time
+from time import sleep
+
+import robot
+
+arlo = robot.Robot()
+
+def drive_eight(n):
+  """ Sends commands to Arlo to drive continuously in an eight-shaped route for n laps. Returns a string on completion."""
+  # Variables for the go_diff function
+  fast_speed = 180
+  slow_speed = 60
+  # Half a lap time, this is the time the robot turns in a direction before switching
+  half_lap_time = 6
+  # To avoid having to manually stop the robot we set it to drive continuously for x amount of seconds.
+  elapsedSecs = 0
+  while elapsedSecs < half_lap_time * 2 * n:
+   arlo.go_diff(fast_speed, slow_speed, 1, 1)
+   sleep(half_lap_time)
+   arlo.go_diff(slow_speed, fast_speed, 1, 1)
+   sleep(half_lap_time)
+   elapsedSecs += half_lap_time * 2
+  return "Arlo ended 'eight' procedure"
+
+
+print drive_eight(10) # Drive 10 eight-shaped laps 
+print arlo.stop()
+
+
--- scripts/squareArlo.py
+++ scripts/squareArlo.py
@@ -3,15 +3,20 @@ from time import sleep
 import robot
 
 arlo = robot.Robot()
+drive_a_meter_time = 5.8
+turn_90_degrees_time = 1.51
 
-#for j in range(4):
-for i in range(4):
-  print arlo.go()
-  sleep(5.8)
-  print arlo.left()
-  sleep(1.51)
-  print arlo.stop()
+def drive_square(straight_time, turn_time)
+  """ Drive in a square, driving straight_time forward and turning for turn_time time (to achieve 90 degrees).
+      We decided to supply these as arguments so that they can easily be tweaked. """
+  # We just repeat the same instructions 4 times to drive in a square
+  for i in range(4):
+    print arlo.go()
+    sleep(straight_time))
+    print arlo.left()
+    sleep(turn_time)
+    print arlo.stop()
 
-#print arlo.go()
-#sleep(5.5)
+drive_square(drive_a_meter_time, turn_90_degrees_time); # Drive in non-continuous square-shape   
+
 arlo.stop()
