80ab2107d2417e47eb39ab012cc89307c1b9bfea
Kallekro <kallekro@gmail.com>
2017-09-09 13:23:53 +0200

    scripts with command line arguments to simplify testing

--- scripts/drive_straight.py
+++ scripts/drive_straight.py
@@ -4,12 +4,18 @@ import robot
 
 import sys
 
-if __name__ == "__main__":
-  drive_time = int(sys.argv[1])
+def main(*arg):
+  if len(arg) != 2:
+    print "The program accepts exactly one argument"      
+  drive_time = int(arg[1])
   if drive_time <= 0:
     drive_time = 1      
   arlo = robot.Robot()
   robot.go()
   sleep(drive_time)
   arlo.stop()
+  
+
+if __name__ == "__main__":
+  main(sys.argv)      
 
--- scripts/turn.py
+++ scripts/turn.py
@@ -4,12 +4,20 @@ import robot
 
 import sys
 
-if __name__ == "__main__":
-  turn_time = int(sys.argv[1])
+def main(*arg):
+  if len(arg) != 3:
+    print "usage: turntime (seconds), direction (0 or 1)"      
+  turn_time = int(arg[1])
   if turn_time <= 0:
     turn_time = 1      
   arlo = robot.Robot()
-  robot.go()
+  if int(arg[2]) == 0:
+    robot.left()
+  else:
+    robot.right()
   sleep(turn_time)
   arlo.stop()
+
+if __name__ == "__main__":
+  main(sys.argv)      
 
