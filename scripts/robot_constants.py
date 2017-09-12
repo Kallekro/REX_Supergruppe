49339929cb1972dd835671b68edec02582d9b291
Kallekro <kallekro@gmail.com>
2017-09-12 15:34:39 +0200

    lol

---
+++ scripts/robot_constants.py
@@ -0,0 +1,10 @@
+# Script for defining robot constants
+# Start by importing this 
+
+one_meter_time = 5.8
+
+turn_90degrees_time = 1 
+
+max_speed = 100
+
+
---
+++ scripts/test_constants.py
@@ -0,0 +1,5 @@
+import robot_constants as rc
+
+print rc.one_meter_time
+print rc.turn_90degrees_time
+print rc.max_speed
