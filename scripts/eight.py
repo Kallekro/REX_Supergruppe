14f18d283e4f0f0f9c5911a3dfd5901e1214305e
Kallekro <kallekro@gmail.com>
2017-09-15 09:27:09 +0200

    Some comments

--- scripts/eight.py
+++ scripts/eight.py
@@ -8,8 +8,8 @@ arlo = robot.Robot()
 def drive_eight(n):
   """ Sends commands to Arlo to drive continuously in an eight-shaped route for n laps. Returns a string on completion."""
   # Variables for the go_diff function
-  fast_speed = 127 
-  slow_speed = 40
+  fast_speed = 80 
+  slow_speed = 25
   # Half a lap time, this is the time the robot turns in a direction before switching
   half_lap_time = 6
   # To avoid having to manually stop the robot we set it to drive continuously for x amount of seconds.
